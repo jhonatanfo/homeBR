c9f9a06d4e9df28923c785d02eddcd39bd51de7c
jhonatan <jnatanfo@hotmail.com>
2022-07-29 00:28:52 -0300

    feat: inicio

---
+++ content/js/padrao.js
@@ -0,0 +1,43 @@
+$(document).on('ready', function() {
+
+    var o = $(".princial-banner");
+    $(document).ready(function() {
+        o.slick({
+            arrows: false,
+            dots: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 500,
+            fade: true,
+            customPaging: function(slider, i) {
+                var thumb = jQuery(slider.$slides[i]).data();
+                return '<a>0' + (i + 1) + '</a>';
+            },
+            cssEase: 'linear'
+        }), o.slickAnimation()
+    });
+
+
+
+});
+
+$(document).ready(function() {
+
+    $(".navbar-nav li a").click(function() {
+        $(".navbar-collapse.collapse").removeClass("in");
+    });
+
+    AOS.init({
+        disable: function() {
+            var maxWidth = 767;
+            return window.innerWidth < maxWidth;
+        }
+    });
+
+    document.addEventListener("mousemove", e => {
+        AOS.init();
+        AOS.refresh();
+    });
+
+});
